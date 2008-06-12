9f753d58a7a23245d3e4af9233b53a0e42f87ebe
wathsala <wathsala@7a49570d-502c-0410-9b4f-bce4f4502a0e>
2008-06-12 10:07:50 +0000

    Nothing interesting

--- bassa_svr/src/bassa_db.c
+++ bassa_svr/src/bassa_db.c
@@ -167,12 +167,13 @@ int bassa_db_init()
     return DB_CREATION_ERROR;
   else
     dbi_result_free(dbres);
+  //printf (">>>>>>>>>>>>>>>>>>>THREAD_SAFE: %i\n", dbi_driver_thread_safe(NULL));
   return SUCCESS;
 }
 
 int bassa_db_reinit()
 {
-  if (!db_conf)
+  /*if (!db_conf)
     return BAD_CONF;
   if (!db_conf->dbicfg)
     return BAD_CONF;
@@ -190,7 +191,7 @@ int bassa_db_reinit()
       	}
     }
   bassa_mutex_unlock (bm);
-  bassa_mutex_free (bm);
+  bassa_mutex_free (bm);*/
   return SUCCESS;
 }
 
@@ -244,10 +245,10 @@ bassa_irequest* bassa_db_getpending()
 	if (bassa_db_reinit())
     return NULL;
   char *sql_query = NULL;
-  const char *temp_str = NULL;
   dbi_result *dbres = NULL;
   bassa_object *bobj = NULL;
   sql_query = "SELECT * FROM cache_index WHERE cache_index.status=\"P\" LIMIT 1";
+  printf ("BASSA_CONN_PTR %p\n", conn);
   dbres = dbi_conn_query (conn, sql_query);
   if (!dbres)
 		{
@@ -258,31 +259,11 @@ bassa_irequest* bassa_db_getpending()
 			if (dbi_result_next_row (dbres))
 				{
 					bobj = bassa_object_new (dbi_result_get_ulong(dbres, "content_length"));
-      		temp_str = dbi_result_get_string (dbres, "origin_url");
-      		if (temp_str)
-        		bobj->origin_url = strdup (temp_str);
-      		else
-        		bobj->origin_url = NULL;
-      		temp_str = dbi_result_get_string (dbres, "status");
-      		if (temp_str)
-        		bobj->status = strdup (temp_str);
-      		else
-        		bobj->status = NULL;
-      		temp_str = dbi_result_get_string (dbres, "object_url");
-      		if (temp_str)
-        		bobj->object_url = strdup (temp_str);
-      		else
-        		bobj->object_url = NULL;
-      		temp_str = dbi_result_get_string (dbres, "object_path");
-      		if (temp_str)
-        		bobj->object_path = strdup (temp_str);
-      		else
-        		bobj->object_path = NULL;
-      		temp_str = dbi_result_get_string (dbres, "file_name");
-      		if (temp_str)
-        		bobj->file_name = strdup (temp_str);
-      		else
-        		bobj->file_name = NULL;
+      		bobj->origin_url = dbi_result_get_string_copy (dbres, "origin_url");
+      		bobj->status = dbi_result_get_string_copy (dbres, "status");
+      		bobj->object_url = dbi_result_get_string_copy (dbres, "object_url");
+      		bobj->object_path = dbi_result_get_string_copy (dbres, "object_path");
+      		bobj->file_name = dbi_result_get_string_copy (dbres, "file_name");
       		bobj->hits = dbi_result_get_ulong (dbres, "hits");
       		dbi_result_free (dbres);
       		bassa_irequest* bir = bassa_irequest_new2 (bobj);
@@ -346,35 +327,14 @@ bassa_object_set *bassa_search_file(char *file_name,
   bassa_object_set *bobjs = (bassa_object_set*)malloc(sizeof(bassa_object_set));
   bobjs->total = dbi_result_get_numrows (dbres);
   bobjs->offset = result_start;
-  const char *temp_str = NULL;
   while (dbi_result_next_row (dbres))
     {
       bobj = bassa_object_new (dbi_result_get_ulong(dbres, "content_length"));
-      temp_str = dbi_result_get_string (dbres, "origin_url");
-      if (temp_str)
-        bobj->origin_url = strdup (temp_str);
-      else
-        bobj->origin_url = NULL;
-      temp_str = dbi_result_get_string (dbres, "status");
-      if (temp_str)
-        bobj->status = strdup (temp_str);
-      else
-        bobj->status = NULL;
-      temp_str = dbi_result_get_string (dbres, "object_url");
-      if (temp_str)
-        bobj->object_url = strdup (temp_str);
-      else
-        bobj->object_url = NULL;
-      temp_str = dbi_result_get_string (dbres, "object_path");
-      if (temp_str)
-        bobj->object_path = strdup (temp_str);
-      else
-        bobj->object_path = NULL;
-      temp_str = dbi_result_get_string (dbres, "file_name");
-      if (temp_str)
-        bobj->file_name = strdup (temp_str);
-      else
-        bobj->file_name = NULL;
+      bobj->origin_url = dbi_result_get_string_copy (dbres, "origin_url");
+      bobj->status = dbi_result_get_string_copy (dbres, "status");
+      bobj->object_url = dbi_result_get_string_copy (dbres, "object_url");
+      bobj->object_path = dbi_result_get_string_copy (dbres, "object_path");
+      bobj->file_name = dbi_result_get_string_copy (dbres, "file_name");
       bobj->hits = dbi_result_get_ulong (dbres, "hits");
       bobjs->bobj[count] = bobj;
       count++;
