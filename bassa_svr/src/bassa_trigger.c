6099036ae9c28b8042fb4b92b4d4d563233ed8cc
wathsala <wathsala@7a49570d-502c-0410-9b4f-bce4f4502a0e>
2008-09-01 10:26:04 +0000

    Event framework

---
+++ bassa_svr/src/bassa_trigger.c
@@ -0,0 +1,91 @@
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/select.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <math.h>
+#include "bassa_trigger.h"
+#include "noc_filter_concur.h"
+
+bassa_trigger* bassa_trigger_new (char *tn)
+{
+  bassa_trigger *bt = (bassa_trigger*)malloc(sizeof(
+	bassa_trigger));
+  bt->fifo_name = tn;
+  int i = mkfifo(bt->fifo_name, 0600);
+  if (i<0)
+  {
+    if (errno == EEXIST)
+      goto fifo_open;
+    perror("MKFIFO");
+    free (bt);
+    bt = NULL;
+    return NULL;
+  }
+
+fifo_open:bt->fifo_fd = open(bt->fifo_name, O_RDWR, 0);
+
+  if (bt->fifo_fd<0)
+  {
+    perror("OPEN");
+    free(bt);
+    bt = NULL;
+    return NULL;
+  }
+  return bt;
+}
+
+void bassa_trigger_free (bassa_trigger *btrig)
+{
+  if (btrig)
+  {
+    close(btrig->fifo_fd);
+    free (btrig);
+    btrig = NULL;
+  }
+}  
+
+int bassa_trigger_sleep (bassa_trigger *t)
+{
+  int rt;
+  int val;
+  fd_set readfs;
+  FD_SET(t->fifo_fd, &readfs);
+SELECT:if ((rt=select(t->fifo_fd+1, &readfs, NULL, NULL, NULL)) < 0)
+  {
+    if (errno != EINTR)
+    {
+      perror("Select");
+      return rt;
+    }
+    else
+      goto SELECT;
+  }
+  else
+  {
+    usleep(500);
+    if((rt=read(t->fifo_fd, &val, sizeof(int)))<0)
+    {
+      perror ("Read");
+      return -1;
+    }
+    else
+      return val;
+  }
+}
+
+int bassa_trigger_wake (bassa_trigger *t, type_t type)
+{
+  int i;
+  if ((i=write(t->fifo_fd, &type, sizeof(type_t))) < 0)
+  {
+    perror("FIFO Problem");
+  }
+  return i;
+}
+
---
+++ bassa_svr/src/bassa_trigger.h
@@ -0,0 +1,30 @@
+#ifndef BASSA_TRIG_H
+#define BASSA_TRIG_H
+#include <time.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/select.h>
+
+#define BASSA_TRANSACTION_COMPLETE 0
+#define BASSA_BAD_SPAWN 1
+#define BASSA_KILL_PROC 2
+#define BASSA_INIT_PROC 3
+#define BASSA_ALARM_FIRED 4
+#define BASSA_DB_UPDATED 5
+
+typedef int type_t;
+
+typedef struct
+{
+  char *fifo_name;
+  int fifo_fd;
+  type_t type;
+}bassa_trigger;
+
+
+bassa_trigger* bassa_trigger_new (char *tn);
+void bassa_trigger_free (bassa_trigger *t);
+int bassa_trigger_sleep (bassa_trigger *t);
+int bassa_trigger_wake (bassa_trigger *t, type_t type);
+
+#endif //BASSA_TRIG_H
